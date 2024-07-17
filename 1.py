f0d0f29979764effcf637ff4e5170bf3047195c5
Lennna <danna.coello@tecnologicoliceocristiano.edu.ec>
2024-07-17 14:02:37 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+
+valorDecimal = float(input("Ingrese un decimal: "))
+valorEntero = int(input("Ingrese un entero: "))
+
+Suma = valorDecimal + valorEntero
+print("El resultado de la suma es "+ str(Suma))
+
+
